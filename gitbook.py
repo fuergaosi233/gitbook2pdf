23941927a79467fc04a8dd08578558cc1e7ba685
廖长江 <1066983132@qq.com>
2019-03-05 22:35:35 +0800

    [feature]获取目录

--- gitbook.py
+++ gitbook.py
@@ -5,6 +5,7 @@ import aiohttp
 import weasyprint
 import datetime
 import os
+import re
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
 from lxml import etree
@@ -58,14 +59,11 @@ class HtmlGenerator():
         return full_html
 
 
-def level(num):
+def get_level_class(num):
     '''
     return 'level'+num
     '''
-    return 'level'+str(num)
-
-
-
+    return 'level' + str(num)
 
 
 class Gitbook2PDF():
@@ -76,7 +74,7 @@ class Gitbook2PDF():
             'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
         }
         self.content_list = []
-        self.heads={'h1':1,'h2':2,'h3':3,'h4':4,'h5':5,'h6':6}
+        self.heads = {'h1': 1, 'h2': 2, 'h3': 3, 'h4': 4, 'h5': 5, 'h6': 6}
         self.meta_list = []
         self.meta_list.append(
             ('generator', 'gitbook2pdf')
@@ -85,35 +83,43 @@ class Gitbook2PDF():
 
     def run(self):
         content_urls = self.collect_urls_and_metadata(self.base_url)
-        content_urls = list(dict.fromkeys(content_urls))  # 去重
-        self.content_list = ["" for _ in range(len(content_urls))]
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.crawl_main_content(content_urls))
-        loop.close()
-
-        # main body
-        body = "".join(self.content_list)
-        # 使用HtmlGenerator类来生成HTML
-        html_g = HtmlGenerator()
-        html_g.add_body(body)
-        for key, value in self.meta_list:
-            html_g.add_meta_data(key, value)
-        html_text = html_g.output()
-        css_text = load_gitbook_css()
-
-        self.write_pdf(self.fname, html_text, css_text)
-
+        # self.content_list = ["" for _ in range(len(content_urls))]
+        # loop = asyncio.get_event_loop()
+        # loop.run_until_complete(self.crawl_main_content(content_urls))
+        # loop.close()
+        #
+        # # main body
+        # body = "".join(self.content_list)
+        # # 使用HtmlGenerator类来生成HTML
+        # html_g = HtmlGenerator()
+        # html_g.add_body(body)
+        # for key, value in self.meta_list:
+        #     html_g.add_meta_data(key, value)
+        # html_text = html_g.output()
+        # css_text = load_gitbook_css()
+        #
+        # self.write_pdf(self.fname, html_text, css_text)
 
     async def crawl_main_content(self, content_urls):
         tasks = []
-        for index, url in enumerate(content_urls):
-            tasks.append(
-                self.gettext(index, url)
-            )
+        for index, urlobj in enumerate(content_urls):
+            if urlobj['url']:
+                tasks.append(self.gettext(index, urlobj['url'], urlobj['level'], urlobj['title']))
+            else:
+                tasks.append(self.getext_fake(index, urlobj['title'], urlobj['level']))
         await asyncio.gather(*tasks)
         print("crawl : all done!")
 
-    async def gettext(self, index, url,baselevel=0):
+    async def getext_fake(self, index, title, level):
+        await asyncio.sleep(0.01)
+        class_ = get_level_class(level)
+        string = "<h1 class='{class}'>{title}</h1>".format_map({
+            'class': class_,
+            'title': title
+        })
+        self.content_list[index] = string
+
+    async def gettext(self, index, url, level, title):
         '''
         return path's html
         '''
@@ -133,9 +139,15 @@ class Gitbook2PDF():
                 context = tree.xpath('//section[@class="normal"]')[0]
             if context.find('footer'):
                 context.remove(context.find('footer'))
-            for head in self.heads:
-                for title in context.xpath(head):
-                    title.attrib['class'] = level(self.heads[head]+baselevel)
+
+            for header in self.heads:
+                if context.xpath(header):
+                    context.xpath(header)[0].attrib['class'] = get_level_class(level)
+                    context.xpath(header)[0].text = title
+                    break
+            # for head in self.heads:
+            #     for title in context.xpath(head):
+            #         title.attrib['class'] = level(self.heads[head] + baselevel)
             text = etree.tostring(context).decode()
             text = html.unescape(text)
             print("done : ", url)
@@ -147,6 +159,9 @@ class Gitbook2PDF():
         tmphtml = weasyprint.HTML(string=html_text)
         tmpcss = weasyprint.CSS(string=css_text)
         fname = os.path.join(os.path.dirname(__file__), 'output', fname)
+        htmlname = os.path.join(os.path.dirname(__file__), 'output', fname).replace('.pdf', '.html')
+        with open(htmlname, 'w', encoding='utf-8') as f:
+            f.write(html_text)
         tmphtml.write_pdf(fname, stylesheets=[tmpcss])
 
     def collect_urls_and_metadata(self, start_url):
@@ -160,6 +175,8 @@ class Gitbook2PDF():
                 title = title_ele.text
                 if '·' in title:
                     title = title.split('·')[1]
+                if '|' in title:
+                    title = title.split('|')[1]
                 title = title.replace(' ', '').replace('/', '-')
                 self.fname = title + '.pdf'
         self.meta_list.append(
@@ -191,18 +208,59 @@ class Gitbook2PDF():
         self.meta_list.append(('dcterms.modified', now))
 
         lis = soup.find('ul', class_='summary').find_all('li')
+        found_urls = []
         content_urls = []
         for li in lis:
             element_class = li.attrs.get('class')
             if not element_class:
                 continue
-            if "chapter" in element_class:
-                data_path = li.attrs.get('data-path')
-                content_urls.append(
-                    urljoin(start_url, data_path)
-                )
+
+            if 'header' in element_class:
+                title = li.text.replace('\n', '').replace(' ', '')
+                data_level = li.attrs.get('data-level')
+                level = len(data_level.split('.')) if data_level else 1
+                content_urls.append({
+                    'url': "",
+                    'level': level,
+                    'title': title
+                })
+
+            elif "chapter" in element_class:
+                data_level = li.attrs.get('data-level')
+                level = len(data_level.split('.'))
+                if 'data-path' in li.attrs:
+                    data_path = li.attrs.get('data-path')
+                    url = urljoin(start_url, data_path)
+                    title = li.find('a').text.replace('\n', '').replace(' ', '')
+                    if url not in found_urls:
+                        content_urls.append(
+                            {
+                                'url': url,
+                                'level': level,
+                                'title': title
+                            }
+                        )
+                        found_urls.append(url)
+
+                # 点击不了的链接
+                else:
+
+                    # 一种获取方式 : http://self-publishing.ebookchain.org/
+                    if li.find('span'):
+                        title = li.find('span').text.replace('\n', '').replace(' ', '')
+                    elif len(li.contents) == 1:
+                        # 只有一个子节点，也就是文字
+                        title = li.text.replace('\n', '').replace(' ', '')
+
+                    content_urls.append({
+                        'url': "",
+                        'level': level,
+                        'title': title
+                    })
+
         return content_urls
 
 
 if __name__ == '__main__':
     Gitbook2PDF("http://self-publishing.ebookchain.org").run()
+    # Gitbook2PDF("https://feisky.xyz/kubernetes-handbook/").run()
