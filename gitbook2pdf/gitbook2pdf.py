f879efd6bcd91b837315da0308e32498bccda938
柚木鉉 <740291272@qq.com>
2021-02-27 12:01:22 +0800

    fix IO_ENCODER

--- gitbook2pdf/gitbook2pdf.py
+++ gitbook2pdf/gitbook2pdf.py
@@ -102,7 +102,7 @@ class ChapterParser():
         if context.find('footer'):
             context.remove(context.find('footer'))
         context = self.parsehead(context)
-        return html.unescape(ET.tostring(context).decode())
+        return html.unescape(ET.tostring(context, encoding='utf-8').decode())
 
     def parsehead(self, context):
         def level(num):
